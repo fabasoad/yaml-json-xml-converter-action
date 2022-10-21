6e3f158ecd332ff3d20f0c8ef78d350bb083ff2f
fabasoad <fabasoad@gmail.com>
2022-10-21 13:21:29 +0900

    Debug

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const core = require('@actions/core');
 const { Converter } = require('./converter');
 
 try {
-  core.notice('NOTICE! test 1');
-  core.warning('Warning! test 2');
-  core.error('ERROR! test 3');
+  core.notice('This GHA is deprecated starting from 2022/10/21', {
+    title: 'Deprecation notice'
+  });
+  core.warning('This GHA is deprecated starting from 2022/10/21', {
+    title: 'Deprecation warning'
+  });
+  core.error('This GHA is deprecated starting from 2022/10/21', {
+    title: 'Deprecation warning'
+  });
   const converter = new Converter();
   core.setOutput('data', converter.convertFile(
     core.getInput('from'),
