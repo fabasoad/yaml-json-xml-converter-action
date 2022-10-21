d9768ebb797a97a73cccdd722e8c061d3f0a1dba
fabasoad <fabasoad@gmail.com>
2022-10-21 13:27:15 +0900

    Debug

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ const core = require('@actions/core');
 const { Converter } = require('./converter');
 
 try {
-  core.notice('This GHA is deprecated starting from 2022/10/21', {
-    title: 'Deprecation notice'
-  });
-  core.warning('This GHA is deprecated starting from 2022/10/21', {
-    title: 'Deprecation warning'
-  });
-  core.error('This GHA is deprecated starting from 2022/10/21', {
-    title: 'Deprecation warning'
-  });
+  const message = 'This GitHub action is deprecated and is not supported star' +
+    'ting from 2022/10/21. Please use https://github.com/fabasoad/data-format' +
+    '-converter-action instead. It is faster, can be run on any OS and suppor' +
+    'ts much more data formats than this one'
+  core.warning(message, { title: 'Deprecation warning' });
   const converter = new Converter();
   core.setOutput('data', converter.convertFile(
     core.getInput('from'),
