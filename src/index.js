663f15b7eb62521660e3b5b69ea551e6d2135658
fabasoad <fabasoad@gmail.com>
2022-10-21 13:19:05 +0900

    Debug

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const core = require('@actions/core');
 const { Converter } = require('./converter');
 
 try {
+  core.notice('NOTICE! test 1');
+  core.warning('Warning! test 2');
+  core.error('ERROR! test 3');
   const converter = new Converter();
   core.setOutput('data', converter.convertFile(
     core.getInput('from'),
