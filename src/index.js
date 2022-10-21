6b477c9fc09148002b0af6d023d123aa0cecc14e
fabasoad <fabasoad@gmail.com>
2022-10-21 13:41:51 +0900

    Update action.yml

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const { Converter } = require('./converter');
 
 try {
   const message = 'This GitHub action is deprecated and is no longer maintain' +
-    'ed starting from 2022/10/21. Please use https://github.com/fabasoad/data' +
-    '-format-converter-action instead. It is faster, can be run on any OS and' +
-    ' supports much more data formats than this one.'
+    'ed starting from 2022/10/21. Please use https://github.com/marketplace/a' +
+    'ctions/data-format-converter-action instead. It is faster, can be run on' +
+    ' any OS and supports much more data formats than this one.'
   core.warning(message, { title: 'Deprecation warning' });
   const converter = new Converter();
   core.setOutput('data', converter.convertFile(
